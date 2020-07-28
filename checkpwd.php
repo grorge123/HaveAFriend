17bdea6fc446578bd096c02ce7d2b8192648f36f
grorge123 <g97116@yahoo.com>
2020-07-28 15:53:59 +0800

    fix fetch_row_to_dic

--- checkpwd.php
+++ checkpwd.php
@@ -2,15 +2,15 @@
 require("dbtools.inc.php");
 require("key.inc.php");
 session_start();
-
 $_SESSION['key']=produce_random();
+echo $_SESSION['key']."<br>";
 $account=$_POST['account'];
 $password=$_POST['password'];
 $username = "";
 $link=create_connection();
 $sql="SELECT * FROM users where account='$account'";
 $result=execute_sql($link,"member",$sql);
-$data = fetch_row_to_dic($result, 'users', 'member');
+$data = fetch_row_to_dic($link, $result, 'member', 'users');
 if($data != NULL && $data['password']==$password){
     $username = $data['name'];
     echo "<h1>Hi</hi>";
@@ -23,7 +23,7 @@ if($data != NULL && $data['password']==$password){
 $method='DES-ECB';
 $str = serialize($username); //将用户信息序列化
 
-$str =openssl_encrypt($str, $method, $randoma,0);
+$str =openssl_encrypt($str, $method, $_SESSION['key'],0);
 echo "用户信息加密后：".$str;
 //将加密后的用户数据存储到cookie中
 setcookie('username', $str);
--- dbtools.inc.php
+++ dbtools.inc.php
@@ -1,15 +1,15 @@
 ﻿<?php
 function create_connection(){
-    $link=mysqli_connect("127.0.0.1","root","") or die("無法連結:1:".mysqli_connect_error());
+    $link=mysqli_connect("127.0.0.1","root","") or die("無法連結mysql:".mysqli_connect_error());
     mysqli_query($link,"SET NAME utf8");
     return $link;
 }  
 function execute_sql($link,$database,$sql){ 
-    mysqli_select_db($link,$database) or die("無法連結:2:".mysqli_error($link)); 
+    mysqli_select_db($link,$database) or die("無法連結database:".mysqli_error($link)); 
     $result=mysqli_query($link,$sql);
     return $result;
 }
-function fetch_row_to_dic($result, $name, $table){
+function fetch_row_to_dic($link, $result, $name, $table){
     $font=mysqli_fetch_row($result);
     $sql = "describe $table";
     $result = execute_sql($link, $name, $sql);
